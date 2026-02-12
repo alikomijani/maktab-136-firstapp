3d4140abc275b3c4b87fb9c4e5c413547a1d43eb
Ali Komijani <a.komijani@wallex.net>
2026-02-12 12:13:21 +0330

    feat: add store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { ErrorBoundary } from "react-error-boundary";
-
+import { Provider } from "react-redux";
 import AppRouter from "./router";
 import { QueryProvider } from "./context/QueryClientProvider";
+import { store } from "./redux/store";
 function App() {
   return (
     <ErrorBoundary fallback={<>خطا در اپلیکیشن</>}>
-      <QueryProvider>
-        <AppRouter />
-      </QueryProvider>
+      <Provider store={store}>
+        <QueryProvider>
+          <AppRouter />
+        </QueryProvider>
+      </Provider>
     </ErrorBoundary>
   );
 }
